feb4ab5e751d2b3ef1c1b453e20c69943b941525
gwatson <glenn@glennwatson.net>
2016-08-16 14:00:27 +1000

    Made a rebase only also perform a force push if one is selected

--- GitSquash.VisualStudio.Extension/GitSquash.VisualStudio.Extension/ViewModel/SquashViewModel.cs
+++ GitSquash.VisualStudio.Extension/GitSquash.VisualStudio.Extension/ViewModel/SquashViewModel.cs
@@ -442,9 +442,33 @@
             return new GitCommandResponse(true, sb.ToString());
         }
 
-        private Task<GitCommandResponse> PerformRebase(CancellationToken token)
+        private async Task<GitCommandResponse> PerformRebase(CancellationToken token)
         {
-            return this.SquashWrapper.Rebase(token, this.SelectedRebaseBranch);
+            var rebaseOutput = await this.SquashWrapper.Rebase(token, this.SelectedRebaseBranch);
+
+            GitCommandResponse forcePushOutput = null;
+            if (this.ForcePush)
+            {
+                forcePushOutput = await this.SquashWrapper.PushForce(token);
+
+                if (forcePushOutput.Success == false || token.IsCancellationRequested)
+                {
+                    return forcePushOutput;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            if (rebaseOutput != null && rebaseOutput.Success)
+            {
+                sb.AppendLine(rebaseOutput.CommandOutput);
+            }
+
+            if (forcePushOutput != null && forcePushOutput.Success)
+            {
+                sb.AppendLine(forcePushOutput.CommandOutput);
+            }
+
+            return new GitCommandResponse(true, sb.ToString());
         }
 
         private Task<GitCommandResponse> PerformAbortRebase(CancellationToken token)
